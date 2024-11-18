c6ccb34059931a6804478c84900f08d81401e346
Биточкин Егор <e.bitochkin@g.nsu.ru>
2024-11-18 22:53:20 +0700

    feat(frontend): create frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-import { LoginButton } from "@telegram-auth/react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <LoginButton
-        botUsername="ooad_project_bot"
-        authCallbackUrl="/path/to/callback/url"
-        buttonSize="large" // "large" | "medium" | "small"
-        cornerRadius={5} // 0 - 20
-        showAvatar={true} // true | false
-        lang="en"
-      />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -39,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
